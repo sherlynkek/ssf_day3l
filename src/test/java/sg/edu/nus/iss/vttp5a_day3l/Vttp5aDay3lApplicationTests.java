8f325e393fef95ec17913f77c6efa8f37efe7b47
sherlynkek <sherlynkek00@gmail.com>
2024-11-20 10:48:45 +0800

    main

---
+++ src/test/java/sg/edu/nus/iss/vttp5a_day3l/Vttp5aDay3lApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_day3l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aDay3lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
